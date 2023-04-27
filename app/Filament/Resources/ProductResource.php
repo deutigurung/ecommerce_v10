594446e016d57a5ddcb5ac16acf3ab440f07c347
Deuti Gurung <gurungdeuti@gmail.com>
2023-04-27 18:30:13 +0545

    product module add via filament

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Forms\Components;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Components\TextInput::make('title')->autofocus()->required(),
+                Components\TextInput::make('price')->numeric()->required(),
+                Components\Select::make('status')->placeholder('Select Status')
+                    ->options([
+                        'active'=>'Active','inactive'=>'Inactive'
+                    ]),
+                Components\Textarea::make('description'),
+                Components\FileUpload::make('image')->directory('products'),
+            ])
+            ->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')->sortable()->searchable(),
+                TextColumn::make('price')->sortable()->searchable(),
+                ImageColumn::make('image'),
+                TextColumn::make('status')->sortable()->searchable()
+
+            ])
+            ->filters([
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }    
+}
--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,10 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            // 'root' => storage_path('app'),
+            'root' => base_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
             'throw' => false,
         ],
 
